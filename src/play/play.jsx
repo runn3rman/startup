6baf279865c5c4392d15531b2a0e71fa980b3092
runn3rman <grantgardner455@gmail.com>
2026-02-12 19:19:02 -0700

    stubs

---
+++ src/play/play.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Play() {
+  return (
+    <main>
+      <div>play displayed here</div>
+    </main>
+  );
+}
---
+++ src/scores/scores.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Scores() {
+  return (
+    <main>
+      <div>scores displayed here</div>
+    </main>
+  );
+}
